8ec162f4275af6e1b9922fb8d6f7d12caa5f6b01
vladpro42 <bondarev.vlad.01@gmail.com>
2025-03-29 13:52:37 +0300

    feat: add toggle menu

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import "./css/style_992.css"
 import "./css/normalize.css"
 
 import './js/module'
+import './js/form'
+import './js/toggleMenu'
---
+++ src/js/toggleMenu.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('.toggle-menu__btn')
+
+if(btn) {
+    btn.addEventListener('click', toogleMenu)
+}
+
+function toogleMenu (event) {
+    const toggleMenu = document.querySelector('.toggle-menu__body')
+
+    event.currentTarget.classList.toggle('toggle-menu__btn--active')
+
+    if(toggleMenu) {
+        toggleMenu.classList.toggle('toggle-menu__body--active')
+    }
+}
